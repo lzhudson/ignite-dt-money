2d1c7aa87ae77deee46bffc53f7c7726ee9aab74
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-28 15:50:49 -0300

    feat(refact): import and usage NewTrasactionModal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 import logo from '../../assets/img/logo.svg';
 import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 export function Header() {
   return <HeaderContainer>
     <HeaderContent>
@@ -10,13 +11,7 @@ export function Header() {
         <Dialog.Trigger asChild>
           <NewTransactionButton>Nova transação</NewTransactionButton>
         </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay />
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-            <Dialog.Close />
-          </Dialog.Content>
-        </Dialog.Portal>
+        <NewTransactionModal />
       </Dialog.Root>
 
     </HeaderContent>
