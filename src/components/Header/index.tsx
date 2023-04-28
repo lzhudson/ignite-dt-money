4840b2fbbf6f7823ab2eb58a8820cd2091db7e21
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-28 15:04:33 -0300

    feat: create modal with Radix Dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 import logo from '../../assets/img/logo.svg';
+import * as Dialog from '@radix-ui/react-dialog';
 export function Header() {
   return <HeaderContainer>
     <HeaderContent>
       <img src={logo} alt="DT Money" />
-      <NewTransactionButton>Nova transação</NewTransactionButton>
+
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          <Dialog.Content>
+            <Dialog.Title>Nova Transação</Dialog.Title>
+            <Dialog.Close />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+
     </HeaderContent>
   </HeaderContainer>
 }
