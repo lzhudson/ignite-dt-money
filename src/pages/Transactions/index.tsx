cfb8af0ec5864104279c2cccb9fa4d5d1f60db3d
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-29 10:19:37 -0300

    feat: get transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./style";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  price: number;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const requestTransactions = await fetch('http://localhost:3000/transactions');
+    const data = await requestTransactions.json();
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <div>
       <Header />
@@ -13,26 +35,18 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+              <td width="50%">{transaction.description}</td>
               <td>
-                <PriceHighlight variant="income">
-                  R$ 12.000,00
+                <PriceHighlight variant={transaction.type}>
+                  {transaction.price}
                 </PriceHighlight>
               </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
             </tr>
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td>
-                <PriceHighlight variant="outcome">
-                  - R$ 59,00
-                </PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            ))}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
