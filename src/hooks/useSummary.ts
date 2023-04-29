8e46364f33c2064a1864d732a7dbc5c44822a869
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-29 10:57:18 -0300

    feat(refact): abstract logic to hook

---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../context/TransactionsContent";
+
+export function useSummary() {
+    const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((acc, currentTransaction) => {
+    if(currentTransaction.type === 'income') {
+      acc.income += currentTransaction.price;
+      acc.total += currentTransaction.price
+    } else if(currentTransaction.type === 'outcome') {
+      acc.outcome += currentTransaction.price;
+      acc.total -= currentTransaction.price
+    }
+    return acc;
+  }, { income: 0, outcome: 0, total: 0 })
+  
+  return summary
+}
